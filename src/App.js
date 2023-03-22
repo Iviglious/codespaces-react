cf303cacc9a2a1dea024362effcb1a7e0ef5ede0
Iviglious <ivo_shalev@yahoo.com>
2023-03-22 10:40:37 +0000

    Added dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { Authenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator
+} from "@aws-amplify/ui-react";
 
 function App() {
   return (
-    <Authenticator>
-    {({ signOut, user }) => (
-      <main>
-        <h1>Hello {user.username}</h1>
-        <button onClick={signOut}>Sign out</button>
-      </main>
-    )}
-    </Authenticator>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      </Authenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import "@aws-amplify/ui-react/styles.css";
+import {
+  defaultDarkModeOverride
+} from '@aws-amplify/ui-react';
+
 Amplify.configure(awsconfig);
+
+export default {
+  name: 'default-theme',
+  overrides: [defaultDarkModeOverride],
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +24,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+/*
+  <ThemeProvider theme={theme} colorMode="system">
+    <App />
+  </ThemeProvider>
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
